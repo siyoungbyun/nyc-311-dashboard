dc16f58e3935dc2b0f04de8494c5ba1079448ee7
Si Young Byun <siyoung.byun@gmail.com>
2020-03-05 19:39:21 +0000

    implemented a small filter

--- dashboard-app/src/components/Dashboard.js
+++ dashboard-app/src/components/Dashboard.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { Row } from "antd";
-import Select from 'react-select';
+import { Row, Col, Select, DatePicker } from "antd";
+import moment from "moment";
+// import Select from 'react-select';
 
-const options = [
-  { value: 'Stanford University', label: 'Stanford' }
-];
+// const options = [
+//   { value: 'Stanford University', label: 'Stanford' }
+// ];
+const Option = Select.Option;
+
+// function handleChange(value) {
+//   console.log(`selected ${value}`);
+// }
+
+function handleBlur() {
+  console.log('blur');
+}
+
+function handleFocus() {
+  console.log('focus');
+}
+
+function handleChange(value) {
+
+  console.log(value); // { key: "lucy", label: "Lucy (101)" }
+}
 
 const field = ({ options }) => (
     <Select
@@ -15,20 +34,51 @@ const field = ({ options }) => (
     />
 );
 
-const Dashboard = ({ children }) => (
+const Dashboard = ({ children, onDateRangeChange }) => (
 
-  <Row
-    type="flex"
-    justify="space-around"
-    align="top"
-    gutter={24}
-    style={{
-      padding: "0 12px 12px 12px",
-      margin: "25px 8px"
-    }}
-  >
-    {children}
-  </Row>
+  <Col gutter={10}>
+    <Row
+      type="flex"
+      justify="space-around"
+      align="top"
+      gutter={24}
+      style={{
+        padding: "0 12px 12px 12px",
+        margin: "25px 8px"
+      }}
+    >
+      {children}
+    </Row>
+    <Row
+      stat
+      align="top"
+      gutter={24}
+      style={{
+        padding: "0 12px 12px 12px",
+        margin: "25px 8px"
+      }}
+      >
+      <Select
+        labelInValue
+        defaultValue={{ key: 'lucy' }}
+        style={{ width: 120 }}
+        onChange={handleChange}
+      >
+        <Option value="jack">Jack (100)</Option>
+        <Option value="lucy">Lucy (101)</Option>
+      </Select>
+    </Row>
+    <Row span={24} lg={6} align="right">
+      <DatePicker.RangePicker
+        disabledDate={d => !d || d.isAfter("2020/02/24") || d.isSameOrBefore("2019/12/30") } format="YYYY-MM-DD"
+        onChange={(date, dateString) => onDateRangeChange(dateString)}
+        defaultValue={[
+          moment("2019/12/31", "YYYY/MM/DD"),
+          moment("2020/02/24", "YYYY/MM/DD")
+        ]}
+      />
+    </Row>
+  </Col>
 );
 
 export default Dashboard;
--- dashboard-app/src/pages/DashboardPage.js
+++ dashboard-app/src/pages/DashboardPage.js
@@ -3,83 +3,152 @@ import { Col } from "antd";
 import ChartRenderer from "../components/ChartRenderer";
 import Dashboard from "../components/Dashboard";
 import DashboardItem from "../components/DashboardItem";
-import Select from "react-select"; // import 'react-select/dist/react-select.css';
 
-const DashboardItems = [
-  {
-    id: 0,
-    name: "The Number of 311 Service Requests by Day",
-    vizState: {
-      query: {
-        filters: [
-          {
-            dimension: "ServiceRequest311.city",
-            operator: "contains",
-            values: ["New"]
-          }
-        ],
-        measures: ["ServiceRequest311.count"],
-        timeDimensions: [
-          {
-            dimension: "ServiceRequest311.createdDate",
-            granularity: "day"
-          }
-        ]
-      },
-      chartType: "main_line"
-    }
-  },
-  {
-    id: 1,
-    name: "Proportion of Request Status",
-    vizState: {
-      query: {
-        filters: [],
-        measures: ["ServiceRequest311.count"],
-        timeDimensions: [],
-        dimensions: ["ServiceRequest311.status"]
-      },
-      chartType: "pie"
-    }
+// const DashboardItems = [
+//   {
+//     id: 0,
+//     name: "The Number of 311 Service Requests by Day",
+//     vizState: {
+//       query: {
+//         filters: [
+//           {
+//             dimension: "ServiceRequest311.city",
+//             operator: "contains",
+//             values: ["New"]
+//           }
+//         ],
+//         measures: ["ServiceRequest311.count"],
+//         timeDimensions: [
+//           {
+//             dimension: "ServiceRequest311.createdDate",
+//             granularity: "day"
+//             // dateRange: this.state.dateRange
+//           }
+//         ]
+//       },
+//       chartType: "main_line"
+//     }
+//   },
+//   {
+//     id: 1,
+//     name: "Proportion of Request Status",
+//     vizState: {
+//       query: {
+//         filters: [],
+//         measures: ["ServiceRequest311.count"],
+//         timeDimensions: [],
+//         dimensions: ["ServiceRequest311.status"]
+//       },
+//       chartType: "pie"
+//     }
+//   }
+// ];
+
+// const DashboardPage = () => {
+//   const dashboardItem = item => (
+//     <Col
+//       span={24}
+//       lg={12}
+//       key={item.id}
+//       style={{
+//         marginBottom: "24px"
+//       }}
+//     >
+//       <DashboardItem title={item.name}>
+//         <ChartRenderer vizState={item.vizState} />
+//       </DashboardItem>
+//     </Col>
+//   );
+
+//   const Empty = () => (
+//     <div
+//       style={{
+//         textAlign: "center",
+//         padding: 12
+//       }}
+//     >
+//       <h2>
+//         There are no charts on this dashboard. Use Playground Build to add one.
+//       </h2>
+//     </div>
+//   );
+
+//   return DashboardItems.length ? (
+//     <Dashboard dashboardItems={DashboardItems}
+//       // onDateRangeChange={dateRange =>
+//       //   this.setState({
+//       //     dateRange: dateRange
+//       //   })
+//       // }
+//     >
+//       {DashboardItems.map(dashboardItem)}
+//     </Dashboard>
+//   ) : (
+//     <Empty />
+//   );
+// };
+
+// export default DashboardPage;
+
+function defaultDate(dateRange) {
+  console.log(dateRange);
+  if (JSON.stringify(dateRange) == JSON.stringify(["", ""])) {
+    console.log(dateRange);
+    var dateRange = ["2019-12-31", "2020-02-24"];
   }
-];
+  console.log(dateRange);
+  return dateRange;
+}
 
-const DashboardPage = () => {
-  const dashboardItem = item => (
-    <Col
-      span={24}
-      lg={12}
-      key={item.id}
-      style={{
-        marginBottom: "24px"
-      }}
-    >
-      <DashboardItem title={item.name}>
-        <ChartRenderer vizState={item.vizState} />
-      </DashboardItem>
-    </Col>
-  );
+class DashboardPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateRange: ["2019-12-31", "2020-02-24"]
+    };
+  }
+  render() {
+    return (
+      <Dashboard
+        onDateRangeChange={dateRange =>
+          this.setState({
+            dateRange: dateRange
+          })
+        }
+      >
+        <Col
+          span={24}
+          lg={12}
+          key={0}
+          style={{
+            marginBottom: "24px"
+          }}
+        >
+          <DashboardItem title={"The Number of 311 Service Requests by Day"}>
+            <ChartRenderer vizState={
+              {query: {
+                filters: [
+                  {
+                    dimension: "ServiceRequest311.city",
+                    operator: "contains",
+                    values: ["New"]
+                  }
+                ],
+                measures: ["ServiceRequest311.count"],
+                timeDimensions: [
+                  {
+                    dimension: "ServiceRequest311.createdDate",
+                    granularity: "day",
+                    dateRange: defaultDate(this.state.dateRange)
+                  }
+                ]
+              },
+              chartType: "main_line"}
+            } />
+          </DashboardItem>
+        </Col>
+      </Dashboard>);
+  }}
 
-  const Empty = () => (
-    <div
-      style={{
-        textAlign: "center",
-        padding: 12
-      }}
-    >
-      <h2>
-        There are no charts on this dashboard. Use Playground Build to add one.
-      </h2>
-    </div>
-  );
-
-  return DashboardItems.length ? (
-    <Dashboard dashboardItems={DashboardItems}>
-      {DashboardItems.map(dashboardItem)}
-    </Dashboard>
-  ) : (
-    <Empty />
-  );
-};
 
 export default DashboardPage;
