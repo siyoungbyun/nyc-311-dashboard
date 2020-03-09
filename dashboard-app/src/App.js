db5500130e696b57550e348dcf3525b039cf42b5
Si Young Byun <siyoung.byun@gmail.com>
2020-03-09 02:15:23 +0000

    Removed token

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -8,9 +8,9 @@ import cubejs from "@cubejs-client/core";
 import { CubeProvider } from "@cubejs-client/react";
 import Header from "./components/Header";
 import WebSocketTransport from "@cubejs-client/ws-transport";
-const API_URL = "http://68.183.27.183:4000";
+const API_URL = "MY_API_URL";
 const CUBEJS_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODMxMDk4NTIsImV4cCI6MTU4MzE5NjI1Mn0.hlV-0r_wQRlaxjaY5rbuAFJmpyvmy_M_TYE1PR6BzKA";
+  "CUBEJS_TOKEN";
 const cubejsApi = cubejs({
   transport: new WebSocketTransport({
     authorization: CUBEJS_TOKEN,
